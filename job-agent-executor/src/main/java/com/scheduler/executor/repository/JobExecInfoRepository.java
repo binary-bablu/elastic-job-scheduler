672a9417144c1ee68464ae584ebc55852850aae5
Thor <binary_bablu@texas.com>
2025-08-28 22:31:29 +0530

    added classes for updating database tables

---
+++ job-agent-executor/src/main/java/com/scheduler/executor/repository/JobExecInfoRepository.java
@@ -0,0 +1,12 @@
+package com.scheduler.executor.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.scheduler.executor.entity.JobExecInfo;
+
+@Repository
+public interface JobExecInfoRepository extends JpaRepository<JobExecInfo, Integer> {
+   
+}
